cad34917de83dc0ec987a0078bd23ccaf2d842e6
NumbShell <acxey32@gmail.com>
2017-02-24 09:22:30 +0100

    Added Website

---
+++ Website/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/add_movie")
+def add_movie():
+    return render_template("add_movie.html")
+
+@app.route("/movies")
+def movies():
+    return render_template("movie_library.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
